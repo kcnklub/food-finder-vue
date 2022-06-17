c63d32a9de4615f369be7c119ae11c45051c1843
Kyle R. Miller <krm8mc@mst.edu>
2022-06-16 21:33:20 -0500

    Adding a unit test and updating cypress test to make sure that tests run and pass in CI/CD pipeline.

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import {currentUserStore} from '@/model/CurrentUser';
 import {useRouter} from "vue-router";
 import axios from "axios";
---
+++ src/model/CurrentGroup.test.ts
@@ -0,0 +1,6 @@
+import {expect, it } from 'vitest'; 
+
+// this is just a test to make sure that the unit tests calls pass in github actions.
+it('Sets Current User', () => {
+    expect(true).equals(true);
+})
