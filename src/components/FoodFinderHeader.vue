36f21d2d917ea9a23e832cfa5442bb065fef4a85
kymiller <kylemiller457@gmail.com>
2022-08-11 21:41:50 -0500

    Getting rid of commit that is useless.

--- src/components/FoodFinderHeader.vue
+++ src/components/FoodFinderHeader.vue
@@ -1,4 +1,3 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <Popover class="relative bg-gradient-to-r from-cyan-500 to-blue-500">
     <div class="max-w-7xl mx-auto px-4 sm:px-6">
